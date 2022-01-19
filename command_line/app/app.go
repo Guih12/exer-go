4258884a38f94530fe0c94c64447864c919322d3
George Borsato <georginhoborsato@gmail.com>
2022-01-19 00:42:07 -0300

    feat: create function list servers

--- command_line/app/app.go
+++ command_line/app/app.go
@@ -13,17 +13,25 @@ func Generate() *cli.App {
 	app.Name = "Aplicação de linha de comando"
 	app.Usage = "Busca de IPS e nome de Servidor na internet"
 
+	flags := []cli.Flag{
+		cli.StringFlag{
+			Name:  "host",
+			Value: "google.com",
+		},
+	}
+
 	app.Commands = []cli.Command{
 		{
-			Name:  "ip",
-			Usage: "Busca de enderecos na net",
-			Flags: []cli.Flag{
-				cli.StringFlag{
-					Name:  "host",
-					Value: "devbook.com.br",
-				},
-			},
+			Name:   "ip",
+			Usage:  "Busca de enderecos na net",
+			Flags:  flags,
 			Action: searchIps,
+		},
+		{
+			Name:   "servidores",
+			Usage:  "Busca o servidores na net",
+			Flags:  flags,
+			Action: searchServer,
 		},
 	}
 	return app
@@ -39,5 +47,17 @@ func searchIps(c *cli.Context) {
 
 	for _, ip := range ips {
 		fmt.Println(ip)
+	}
+}
+
+func searchServer(c *cli.Context) {
+	host := c.String("host")
+	servers, err := net.LookupNS(host)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	for _, server := range servers {
+		fmt.Println(server)
 	}
 }
